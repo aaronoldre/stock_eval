1b591a55877c573dee6a1484df45d68e90540ae2
Aoldre <aaronoldre@gmail.com>
2024-12-28 00:53:11 +0000

    data frame controls

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 from datetime import datetime 
 import streamlit as st
 import pandas as pd
-import yfinance as yf
+##import yfinance as yf
 
 # Title
 st.title(":dollar: Stock as Compenstation Evaluator :chart:")
@@ -9,21 +9,32 @@ st.write("use to find high points in RSUs and Options when they vest")
 
 # prompt user for stocks and write to data frame
 df = pd.DataFrame({'Ticker', 'Grant Date', 'Num Shares', 'Grant Type', 'Current Value'})
+if not df.empty:
+    st.dataframe(df)
+
+
 ticker = st.text_input('Enter Stock Ticker')
 start_date = st.date_input('Stock Grant Date')
+num_shares = st.number_input('Enter Number of Shares', 0)
+grant_type = st.selectbox('Grant Type', ['RSUs', 'Options'])
+if st.button('Add Stock'):
+    new_row = {'Ticker':ticker, 'Grant Date':start_date, 'Num Shares':num_shares, 'Grant Type':grant_type}
+    df = df.append(new_row, ignore_index=True)
+    st.write('second time')
+    st.dataframe(df)
 # loop back until all options added button pressed
 
 # pull up ticker prices
 # start_date = datetime(2020, 1, 1) 
 end_date = datetime.today()
 
-try:
-    stock_data = yf.download(ticker, start = start_date, 
-				end = end_date) 
+# try:
+#     stock_data = yf.download(ticker, start = start_date, 
+# 				end = end_date) 
     
-    st.line_chart(stock_data)
-except:
-    st.write('Enter values to seach')
+#     st.line_chart(stock_data)
+# except:
+#     st.write('Enter values to seach')
 # math 
 
 # chart worth, and number of shares, find best day to have sold
